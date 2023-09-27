6e9305cc7ef1968c002918d3f8762639ad6e9c2a
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-09-27 17:05:25 +0400

    create authUser interface for signin and signup components

---
+++ src/components/user-auth/Auth.types.d.ts
@@ -0,0 +1,6 @@
+// User name description, Because the username is used in the registration and authentication components,
+// it's better to describe it separately to avoid duplication
+
+export interface AuthUser {
+    name: string
+}
